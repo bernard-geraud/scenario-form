9f502d7361eb72400b61286df8685451809c41bb
bernard-geraud <bernard.dongmo@kaeyros-analytics.com>
2024-02-12 13:27:07 +0100

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    apiUrl: 'https://9bbsl0vwr0.execute-api.eu-central-1.amazonaws.com'
+    // apiUrl: 'http://localhost:4000'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    apiUrl: 'https://9bbsl0vwr0.execute-api.eu-central-1.amazonaws.com'
+};
